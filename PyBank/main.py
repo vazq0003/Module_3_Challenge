5699d08b0ca320e17e0121378e9500584860e975
Tristan Vazquez <tvazquez1122@gmail.com>
2023-07-19 21:40:00 -0500

    Added in comments

--- PyBank/main.py
+++ PyBank/main.py
@@ -2,19 +2,23 @@
 import os
 import csv 
 
+#set paths
 csvbankpath = os.path.join('Resources','budget_data.csv')
 bankoutput_path = os.path.join('Analysis','bank_main.txt')
 
+#set variables
 monthscount = []
 totalprofitloss = []
 profitlosschange = []
 avg_change = 0 
 
+#open and read csv
 with open(csvbankpath) as bankcsvfile:
     bankcsvreader = csv.reader(bankcsvfile, delimiter=",")
 
     header = next(bankcsvreader)
 
+    #loops to populate lists for calculations
     for row in bankcsvreader:
 
         monthscount.append(row[0])
@@ -23,16 +27,19 @@ with open(csvbankpath) as bankcsvfile:
     for i in range(0,85):
         profitlosschange.append(totalprofitloss[i+1] - totalprofitloss[i])
 
+#calculate max increse/decrease and average change
 max_increase_profitloss = max(profitlosschange)
 max_decrease_profitloss = min(profitlosschange)
 avg_change = round(sum(profitlosschange)/len(profitlosschange),2)
 
+#locate months in which max increse/decrease happened
 location_max_increase = profitlosschange.index(max_increase_profitloss)
 location_max_decrease = profitlosschange.index(max_decrease_profitloss)
 
 max_increase_month = monthscount[location_max_increase + 1]
 max_decrease_month = monthscount[location_max_decrease + 1]
 
+#print requested values
 print('Total Months: ' + str(len(monthscount)))
 print('Total Profit/Loss: ' + str(sum(totalprofitloss)))
 print('Average Change: ' + str(avg_change))
@@ -40,6 +47,7 @@ print('Greatest Increase in Profits:' + ' ' + max_increase_month + ' ' + str(max
 print('Greatest Decrease in Profits:' + ' ' + max_decrease_month + ' ' + str(max_decrease_profitloss))
 
 
+#write answers to text file
 with open(bankoutput_path, 'w') as file:
     file.write('Financial Analysis')
     file.write("\n")
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -2,19 +2,23 @@
 import os
 import csv 
 
+#set paths
 csvpollpath = os.path.join('Resources','election_data.csv')
 polloutput_path = os.path.join('Analysis','poll_main.txt')
 
+#set variables
 vote_count = []
 vote_stockham = []
 vote_degette = []
 vote_doane = [] 
 
+#open and read csv
 with open(csvpollpath) as pollcsvfile:
     pollcsvreader = csv.reader(pollcsvfile,delimiter=",")
 
     header = next(pollcsvreader)
 
+    #loop and conditionals to populate lists
     for row in pollcsvreader:
 
         vote_count.append(row[0])
@@ -26,6 +30,7 @@ with open(csvpollpath) as pollcsvfile:
         if row[2] == "Raymon Anthony Doane":
             vote_doane.append(row[0])
 
+#calculate votes and percent of votes
 total_vote = len(vote_count)
 total_stockham = len(vote_stockham)
 total_degette = len(vote_degette)
@@ -35,16 +40,19 @@ pct_stockham = total_stockham/total_vote
 pct_degette = total_degette/total_vote
 pct_doane = total_doane/total_vote
 
+#create dictionary and locate winner
 candidate_dictionary = {"Charles Casper Stockham": total_stockham,"Diana DeGette": total_degette,"Raymon Anthony Doane": total_doane}
 
 winner_key = max(candidate_dictionary, key=candidate_dictionary.get)
 
+#print results
 print(f'Total Votes: {total_vote}')
 print(f'Charles Casper Stockham: {pct_stockham:.2%} ({total_stockham})')
 print(f'Diana DeGette: {pct_degette:.2%} ({total_degette})')
 print(f'Raymon Anthony Doane: {pct_doane:.2%} ({total_doane})')
 print(f'Winner: {winner_key}')
 
+#write results to text file
 with open(polloutput_path, 'w') as file:
     file.write('Election Results')
     file.write("\n")
